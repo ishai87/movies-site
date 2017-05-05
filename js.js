b0758ec48fd0a7df13606a4392ab3400f423405d
ishai87 <levishai87@gmail.com>
2017-05-05 17:09:11 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -3,13 +3,13 @@ var currentYear= new Date();
 b1.onclick=function() {
 
   var byear=parseInt(document.getElementById("byinput").value);
-  if (byear<1000){
+  if (byear<1900){
     window.alert("Minimum birth year is 1900. please insert valid year of birth");
   }
   if (byear>currentYear.getFullYear()){
-    window.alert("No way that you where born in the future!!! please insert valid birth date");
+    window.alert("No way that you were born in the future!!! please insert valid birth date");
   }
-  if(byear>=1000 & byear<=currentYear.getFullYear()){
+  if(byear>=1900 & byear<=currentYear.getFullYear()){
     if (currentYear.getFullYear()-byear>=13) {
       window.location.href="adultpage.html";
     }
